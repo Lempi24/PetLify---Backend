ed83e5d806c451f9dc7c8d8f1aef9424adb496d2
oswitek <oliwierswitek@gmail.com>
2025-10-20 19:34:11 +0200

    Refactor createPetProfile indentation and formatting
    Reformatted the createPetProfile controller for improved readability by adjusting indentation and code structure. No functional changes were made.

--- controllers/petProfileController.js
+++ controllers/petProfileController.js
@@ -3,45 +3,45 @@ import cloudinary from '../cloudinary.js';
 import fs from 'fs';
 
 export const createPetProfile = async (req, res) => {
-  try {
-    const { user } = req.user;
-    const { petId, petName, petAge, petSize, petSpecies, petBreed, petColor } = req.body;
+    try {
+        const { user } = req.user;
+        const { petId, petName, petAge, petSize, petSpecies, petBreed, petColor } = req.body;
 
-    const existingPetProfiles = await fetchPetProfile(req, res);
+        const existingPetProfiles = await fetchPetProfile(req, res);
 
-    if (existingPetProfiles && existingPetProfiles.length > 2) {
-      return res.status(400).json({ message: 'User has reached the maximum number of pet profiles' });
-    }
+        if (existingPetProfiles && existingPetProfiles.length > 2) {
+        return res.status(400).json({ message: 'User has reached the maximum number of pet profiles' });
+        }
 
-    let photo_urls = [];
+        let photo_urls = [];
 
-    if (req.files.length > 5) {
-        return res.status(400).json({ message: 'You can upload up to 5 photos only' });
-    }
+        if (req.files.length > 5) {
+            return res.status(400).json({ message: 'You can upload up to 5 photos only' });
+        }
 
-    if (req.files && req.files.length > 0) {
-        const results = await Promise.all(
-            req.files.map((file) =>
-                cloudinary.uploader.upload(file.path, { folder: 'pet_profiles_photos' })
-            )
+        if (req.files && req.files.length > 0) {
+            const results = await Promise.all(
+                req.files.map((file) =>
+                    cloudinary.uploader.upload(file.path, { folder: 'pet_profiles_photos' })
+                )
+            );
+            photo_urls = results.map((r) => r.secure_url);
+            req.files.forEach((f) => fs.unlinkSync(f.path));
+        } else {
+            return res.status(400).send('No photo uploaded');
+        }
+        
+        await pool.query(
+        `INSERT INTO pets_info.pet_profiles (id, owner, pet_name, pet_age, pet_size, pet_species_type, pet_breed, pet_color, photo_url) 
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`,
+        [petId, user.email, petName, petAge, petSize, petSpecies, petBreed, petColor, photo_urls]
         );
-        photo_urls = results.map((r) => r.secure_url);
-        req.files.forEach((f) => fs.unlinkSync(f.path));
-    } else {
-        return res.status(400).send('No photo uploaded');
-    }
-    
-    await pool.query(
-      `INSERT INTO pets_info.pet_profiles (id, owner, pet_name, pet_age, pet_size, pet_species_type, pet_breed, pet_color, photo_url) 
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`,
-      [petId, user.email, petName, petAge, petSize, petSpecies, petBreed, petColor, photo_urls]
-    );
 
-    res.status(200).json({ message: 'Pet profile created successfully' });
-  } catch (error) {
-    console.error('Error creating pet profile:', error);
-    res.status(500).json({ message: 'Internal server error' });
-  }
+        res.status(200).json({ message: 'Pet profile created successfully' });
+    } catch (error) {
+        console.error('Error creating pet profile:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
 };
 
 export const fetchPetProfile = async (req, res) => {
