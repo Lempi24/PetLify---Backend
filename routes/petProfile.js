195366bd98de575b1aab199dbec7b8146815b98c
oswitek <oliwierswitek@gmail.com>
2025-10-20 19:00:55 +0200

    Add authentication and delete route to petProfile
    Protected the create and fetch pet profile routes with token authentication and added a new route for deleting a pet profile.

--- routes/petProfile.js
+++ routes/petProfile.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { createPetProfile, fetchPetProfile } from '../controllers/petProfileController.js';
+import authenticateToken from '../tokenAuthorization.js';
+import { createPetProfile, fetchPetProfile, deletePetProfile } from '../controllers/petProfileController.js';
 
 const router = express.Router();
 
-router.post('/createPetProfile', createPetProfile);
-router.get('/fetchPetProfile', fetchPetProfile);
+router.post('/createPetProfile', authenticateToken, createPetProfile);
+router.get('/fetchPetProfile', authenticateToken, fetchPetProfile);
+router.delete('/deletePetProfile', authenticateToken, deletePetProfile);
 
 export default router;
